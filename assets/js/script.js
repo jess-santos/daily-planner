ce234ef0ca868b3f83034922f6fafb6433e45c81
[jess santos] <jessxsantos@icloud.com>
2024-03-22 16:55:24 +0000

    Edited styling for button

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,14 @@ function generateTimeBlocks() {
     const container = $('.container');
     for (let i = 9; i <= 17; i++) {
         const timeBlock = `
-            <div class="row">
-                <div class="col-2">${i}:00</div>
-                <div class="col-8"><textarea class="event-input" id="event-input-${i}"></textarea></div>
-                <div class="col-2"><button class="save-btn">Save</button></div>
-            </div>
-        `;
+        <div class="row">
+        <div class="col-2">${i}:00</div>
+        <div class="col-8"><textarea class="event-input" id="event-input-${i}"></textarea></div>
+        <div class="col-2">
+            <button class="save-btn"><i class="fas fa-save"></i></button> 
+        </div>
+    </div>
+`;
         container.append(timeBlock);
     }
 }
