cdb2872640da8f740d0491d1bbc26fbd7e53c628
[jess santos] <jessxsantos@icloud.com>
2024-03-22 16:00:31 +0000

    Function to create the time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,17 @@ $(document).ready(function() {
     loadSavedEvents();
 });
 
+function generateTimeBlocks() {
+    const container = $('.container');
+    for (let i = 9; i <= 17; i++) {
+        const timeBlock = `
+            <div class="row">
+                <div class="col-2">${i}:00</div>
+                <div class="col-8"><textarea class="event-input" id="event-input-${i}"></textarea></div>
+                <div class="col-2"><button class="save-btn">Save</button></div>
+            </div>
+        `;
+        container.append(timeBlock);
+    }
+}
 
